7cb690674cd059a85525b4f219137ec97e5175cb
Monica <moni21889@gmail.com>
2023-05-04 15:25:37 -0500

    Se crea vista principal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import Form from "./components/Form";
+import ItemList from "./components/ItemList";
+import { nanoid } from "nanoid";
 
-function App() {
+function App(props) {
+
+const [tasks, setTasks] = useState(props.tasks);
+
+const taskList = tasks.map((task) => (
+    <h2>
+      {task.name}
+    </h2>
+  ));
+
+  const tasksNoun = taskList.length !== 1 ? "Tareas Ingresadas" : "Tarea Ingresada";
+  const headingText = taskList.length !== 0 ? `${taskList.length} ${tasksNoun} ` : "No hay tareas ingresadas";
+
+  const listHeadingRef = useRef(null);
+  const prevTaskLength = usePrevious(tasks.length);
+
+  function usePrevious(value) {
+    const ref = useRef();
+    useEffect(() => {
+      ref.current = value;
+    });
+    return ref.current;
+  }
+
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [tasks.length, prevTaskLength]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. tesd
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <form>
+    <html>
+      <body  >
+        <div className = "main">
+          <div className="container">
+            <Form />
+            <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>
+              {headingText}
+            </h2>
+            <ul
+              className="todo-list stack-large stack-exception"
+              aria-labelledby="list-heading">
+              {taskList}
+            </ul>
+          </div>
+        </div>
 
-      </header>
-    </div>
+      </body>
+    </html>
   );
 }
 
